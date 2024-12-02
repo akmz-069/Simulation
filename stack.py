effb095373890d9b24b9a051ab32ebc32af6b301
akmz-069 <aayush0mulmi@gmail.com>
2024-12-02 22:45:16 +0545

    modified the code and added comments which are clear

--- stack.py
+++ stack.py
@@ -1,10 +1,11 @@
 class Stack:
     def __init__(self):
         self.items = []
-
+        
+    # To append items into the stack
     def push(self, item):
         self.items.append(item)
-
+    # To Remove the items from ths stack
     def pop(self):
         if not self.is_empty():
             return self.items.pop()
@@ -12,7 +13,7 @@ class Stack:
 
     def peek(self):
         return self.items[-1] if not self.is_empty() else "Stack is empty"
-
+    #checks if the stack is empty 
     def is_empty(self):
         return len(self.items) == 0
 
